6e5fea654044ed5891ca3c6680b662baac8c132b
Malathi <malathi.yogesh@gmail.com>
2017-02-22 16:45:26 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,27 @@ var button = document.getElementById('counter');
 var counter = 0;
 
 button.onclick = function () {
-    //Make  a request to the counter endpoint
+    
+    //Create a request
+    var request = new XMLHttpRequest();
     
     //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                 var counter = request.responseText;
+                 //Render the variable in the correct span
+                 var span = document.getElementById('count');
+                 span.innerHTML = counter.toString();
+            }
+        }
+    };
     
-    //Render the variable in the correct span
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    //Mkae the request
+    request.Open('GET', 'http://malathiyogesh.imad.hasura-app.io/counter');
+    request.send(null);
+  
     
     
 };
