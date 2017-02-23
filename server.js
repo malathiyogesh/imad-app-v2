52c6cfd07ee62b953e548f2d9e6c42938769d584
Malathi <malathi.yogesh@gmail.com>
2017-02-23 10:53:42 +0800

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/counter', function(req, res) {
     counter = counter + 1;
     res.send(counter.toString());
 });
-app.get('/submit-name/:name', function(req, res){
+app.get('/submit-name/', function(req, res){
     //Get the name from the request
     //var name = req.params.name;
     var name = req.query.name;
