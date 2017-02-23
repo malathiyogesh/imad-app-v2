61d9195a243ce1aa772c002d2989cd27e27f9d73
Malathi <malathi.yogesh@gmail.com>
2017-02-23 11:01:11 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,15 +30,30 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
-    //Make a request to the  server and send the name
+   //Create a request
+    var request = new XMLHttpRequest();
+    
+    //Capture the response and store it in a variable
+    request.onreadystatechange = function() {
+        if(request.readystate === XMLHttpRequest.DONE){
+            //Take some action
+            if(request.status === 200){
+                var names = ['name1', 'name2', 'name3', 'name4'];
+                var list ='';
+                for(var i=0; i < names.length; i++){
+                    list += '<li>' + names[i] + '</li>';
+                }
+            var ul = document.getElementById('namelist');
+            ul.innerHtml = list;
+            }
+        }
+    };
+    
+    //Make the request
+    request.Open('GET', 'http://malathiyogesh.imad.hasura-app.io/submit-name?name=' + name , true);
+    request.send(null);
     
     //Capture the list of names and render it as  a list
-    var names = ['name1', 'name2', 'name3', 'name4'];
-    var list ='';
-    for(var i=0; i < names.length; i++){
-        list += '<li>' + names[i] + '</li>';
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHtml = list;
+   
 
 };
