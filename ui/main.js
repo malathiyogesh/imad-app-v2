1afa62060029e1a29828794979dff13fc6ba3c48
Malathi <malathi.yogesh@gmail.com>
2017-02-23 11:04:46 +0800

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,8 +26,7 @@ button.onclick = function () {
 };
 
 //Submit name
-var nameInput = document.getElementById('name');
-var name = nameInput.value;
+
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
    //Create a request
@@ -49,7 +48,8 @@ submit.onclick = function () {
             }
         }
     };
-    
+    var nameInput = document.getElementById('name');
+    var name = nameInput.value;
     //Make the request
     request.Open('GET', 'http://malathiyogesh.imad.hasura-app.io/submit-name?name=' + name , true);
     request.send(null);
